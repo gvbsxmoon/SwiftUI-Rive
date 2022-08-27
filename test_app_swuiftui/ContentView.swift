ea884fbd8e87995222ad00453626778740a9f47e
gvbsxmoon <lucanatale75@gmail.com>
2022-08-27 13:04:15 +0200

    Initial Commit

---
+++ test_app_swuiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test_app_swuiftui
+//
+//  Created by Luca Natale on 27/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test_app_swuiftui/test_app_swuiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_app_swuiftuiApp.swift
+//  test_app_swuiftui
+//
+//  Created by Luca Natale on 27/08/22.
+//
+
+import SwiftUI
+
+@main
+struct test_app_swuiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
